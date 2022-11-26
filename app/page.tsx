467f5adf309b7ddd6101bf85152fa458653bf8b7
Indra Zulfi <indrazulfi@proton.me>
2022-11-26 22:09:23 +0700

    Basic Chroma added

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,38 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import chroma from "chroma-js";
+import { useState } from "react";
 
 export default function Home() {
+  const [palette, setPalette] = useState<string[]>([]);
+
+  const generatePalette = () => {
+    const randomColor1 = chroma.random().hex();
+    const randomColor2 = chroma.random().hex();
+    const generatedPalette = chroma
+      .bezier([randomColor1, randomColor2])
+      .scale()
+      .colors(5);
+    setPalette(generatedPalette);
+  };
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+    <div className="flex w-full h-screen justify-center items-center">
+      <div>
+        <div className="flex">
+          {palette.map((item) => {
+            return (
+              <div
+                className="min-w-[120px] min-h-[120px]"
+                style={{ backgroundColor: item }}
+              ></div>
+            );
+          })}
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        <div>
+          <button onClick={generatePalette}>Generate</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
